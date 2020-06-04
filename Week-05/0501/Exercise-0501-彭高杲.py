19ce88dbc7c6899b160727514ef0f7f088f10145
Silvia-011 <61576882+Silvia-011@users.noreply.github.com>
2020-06-04 16:15:01 +0800

    彭高杲（0501多页读取更新）

--- Week-05/0501/Exercise-0501-彭高杲.py
+++ Week-05/0501/Exercise-0501-彭高杲.py
@@ -39,37 +39,41 @@ f.write("共有："+ques_num)
 
 i = 1
 
-#尝试多页读取，暂未果
+# 尝试多页读取
 
-'''
+# 计算总共需要拉取的次数
 total_num = int(re.search(r"[1-9]\d*", str(ques_num)).group())
 total_num = math.floor(total_num/5)
+
 for z in range(total_num):
+
+    for q in range(5):
+        answer = browser.find_element_by_css_selector(
+            "#QuestionAnswers-answers > div > div > div > div:nth-child(2) > div > div:nth-child(%d)" % i)
+        f.write("\n")
+        f.write("\n回答%d" % i)
+        author = answer.find_element_by_class_name('AuthorInfo-name')
+        if author_link := author.find_elements_by_class_name("UserLink-link"):
+            author_name = author.find_element_by_class_name("UserLink-link").text
+            f.write("\n作者：" + author_name)
+        else:
+            f.write("\n作者：匿名用户")
+        like = answer.find_element_by_class_name("Voters")
+        like = like.find_element_by_class_name("Button--plain").text
+        like = re.search(r"[1-9]\d*", str(like)).group()
+        f.write("\n赞同：" + like + "\n")
+        for ans in answer.find_elements_by_class_name("CopyrightRichText-richText"):
+            ans = ans.text
+            ans = ans.encode("utf-8")
+            ans = ans.decode("utf-8")
+            f.write("\n" + ans)
+        f.write("\n")
+        i += 1
+
+    # 每次读取完当前界面，向下拉取获得更多回答
     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     browser.find_element_by_xpath('//body[@class="nimbus-is-editor"]').send_keys(Keys.DOWN)
-    time.sleep(5)
-    '''
-
-for q in range(5):
-    answer = browser.find_element_by_css_selector(
-        "#QuestionAnswers-answers > div > div > div > div:nth-child(2) > div > div:nth-child(%d)" % i)
-    f.write("\n")
-    f.write("\n回答%d" % i)
-    author = answer.find_element_by_class_name('AuthorInfo-name')
-    author = author.find_element_by_class_name("UserLink-link").text
-    f.write("\n作者：" + author)
-    like = answer.find_element_by_class_name("Voters")
-    like = like.find_element_by_class_name("Button--plain").text
-    like = re.search(r"[1-9]\d*", str(like)).group()
-    f.write("\n赞同：" + like + "\n")
-    for ans in answer.find_elements_by_class_name("CopyrightRichText-richText"):
-        ans = ans.text
-        ans = ans.encode("utf-8")
-        ans = ans.decode("utf-8")
-        f.write("\n" + ans)
-    f.write("\n")
-    i += 1
-
+    time.sleep(10)
 
 f.write("======================完毕======================")
 f.close()
